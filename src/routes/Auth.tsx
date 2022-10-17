0ee9a3b7407a273886b3a78c4460ec5c064b304c
kylie <wlswn4630@gmail.com>
2022-10-17 17:27:40 +0900

    feat: create login form

--- src/routes/Auth.tsx
+++ src/routes/Auth.tsx
@@ -1,5 +1,28 @@
-import React from "react";
+import React, {ChangeEvent, useState} from "react";
 
 export const Auth = () => {
-    return <>Auth</>
+    const [email, setEmail] = useState<string>("");
+    const [password, setPassword] = useState<string>("");
+
+    const onChange = (e: ChangeEvent<HTMLInputElement>) => {
+        const {name, value} = e.currentTarget;
+        name === 'email' ? setEmail(value) : setPassword(value);
+    }
+    const onSubmit = (e: ChangeEvent<HTMLFormElement>) => {
+        e.preventDefault();
+    }
+
+    return <div>
+        <form>
+            <input placeholder={'Enter your Email address'} name={'email'} type={'email'} onChange={onChange}
+                   required={true}/>
+            <input placeholder={'Enter your Email address'} name={'password'} type={'password'} onChange={onChange}
+                   required={true}/>
+            <input type={'submit'} value={'Log In'}/>
+        </form>
+        <div>
+            <button>Continue with Google</button>
+            <button>Continue with GitHub</button>
+        </div>
+    </div>
 }
