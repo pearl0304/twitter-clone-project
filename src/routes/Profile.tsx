4c8bef0d1619aaff01ec606d136b47e54dee19f3
Kylie <kylie@comets.kr>
2022-10-19 15:15:08 +0900

    feat: log Out

--- src/routes/Profile.tsx
+++ src/routes/Profile.tsx
@@ -1,5 +1,21 @@
 import React from "react";
+import {firebaseAuth} from "../initFirebase";
+import {signOut} from "firebase/auth";
+import { useNavigate } from 'react-router-dom';
+/** Link : 특정 주소로 이동해주는 태그
+ * useNavigate : 특정 행동을 했을 때 해당 주소로 이동
+ * **/
 
 export const Profile = () => {
-    return <></>
+    const navigate = useNavigate();
+    const onLogOutClick = () => {
+        signOut(firebaseAuth).then(() => {
+            navigate('/')
+        }).catch((error) => {
+            console.log(error)
+        });
+    }
+    return <>
+        <button onClick={onLogOutClick}>Log Out</button>
+    </>
 }
--- src/routes/Router.tsx
+++ src/routes/Router.tsx
@@ -2,14 +2,19 @@ import React, {useState} from "react";
 import {HashRouter as Router, Routes, Route} from "react-router-dom";
 import {Auth} from "./Auth";
 import {Home} from "./Home";
+import {Profile} from "./Profile";
+import {Navigation} from "../components/Navigation";
 
 // @ts-ignore
 export const AppRouter = ({isLoggedIn}) => {
     return (
         <Router>
+            {isLoggedIn && <Navigation/>}
             <Routes>
                 {isLoggedIn ? <>
                     <Route path='/' element={<Home/>}></Route>
+                    <Route path='/profile' element={<Profile/>}></Route>
+
                 </> : <Route path='/' element={<Auth/>}></Route>}
             </Routes>
         </Router>
